beab32334bb3f539ef36fd73388c35c2ad1c390f
Nugroho Herucahyono <xinuc@xinuc.org>
2010-03-22 20:38:31 +0700

    modify test, pass with rails 3 beta

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,26 @@ require 'rubygems'
 
 require 'test/unit'
 require 'action_controller'
-
 require 'action_controller/test_case'
+
 require File.join(File.dirname(__FILE__), '..', 'init.rb')
 
+ActionController::Routing::Routes.draw do |map|
+  match "/index" => "ekuseru#index"
+end
 
-ActionController::Routing::Routes.reload rescue nil
+class ActiveSupport::TestCase
+end
+
+module ActionController
+  class TestRequest < ActionDispatch::TestRequest
+    def flash
+      {}
+    end
+  end
+end
+
+class Hash
+  def sweep
+  end
+end
